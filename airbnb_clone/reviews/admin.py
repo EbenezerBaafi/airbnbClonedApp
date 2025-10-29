922c39084c693aacbd204d2712938e34282d6202
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-29 00:22:58 +0000

    Added the admin for the review

--- airbnb_clone/reviews/admin.py
+++ airbnb_clone/reviews/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Review
 
-# Register your models here.
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    """Admin interface for Review model"""
+    list_display = ['reviewer', 'listing', 'rating', 'average_rating', 'created_at']
+    list_filter = ['rating', 'created_at']
+    search_fields = ['reviewer__username', 'listing__title', 'comment']
+    date_hierarchy = 'created_at'
+    
+    fieldsets = (
+        ('Review Info', {
+            'fields': ('booking', 'listing', 'reviewer')
+        }),
+        ('Ratings', {
+            'fields': ('rating', 'cleanliness', 'communication', 'check_in', 
+                      'accuracy', 'location', 'value')
+        }),
+        ('Comments', {
+            'fields': ('comment', 'host_response')
+        }),
+    )
+    
+    readonly_fields = ['booking', 'listing', 'reviewer']
