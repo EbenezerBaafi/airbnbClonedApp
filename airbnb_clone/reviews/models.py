91049947f2b709d49b2d53c1a84a20cbad40a531
Aled Ebenezer Baafi Junior <cedicray20@gmail.com>
2025-10-29 00:55:05 +0000

    Added models for reviews

--- airbnb_clone/reviews/models.py
+++ airbnb_clone/reviews/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from django.conf import settings
+from django.core.validators import MinValueValidator, MaxValueValidator
+from listings.models import Listing
+from bookings.models import Booking
 
-# Create your models here.
+class Review(models.Model):
+    """Model for property reviews"""
+    booking = models.OneToOneField(Booking, on_delete=models.CASCADE, related_name='review')
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name='reviews')
+    reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviews')
+    
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    cleanliness = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    communication = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    check_in = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    accuracy = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    location = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    value = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    
+    comment = models.TextField()
+    host_response = models.TextField(blank=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return f"Review by {self.reviewer.username} for {self.listing.title}"
+    
+    @property
+    def average_rating(self):
+        return (self.cleanliness + self.communication + self.check_in + 
+                self.accuracy + self.location + self.value) / 6
+    
+    class Meta:
+        ordering = ['-created_at']
